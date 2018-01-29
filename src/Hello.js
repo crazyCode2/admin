5359725035105b36bb79673800c2fe2661f94229
crazyCode2 <1178770858@qq.com>
2018-01-29 18:10:52 +0800

    add new

--- src/Hello.js
+++ src/Hello.js
@@ -2,13 +2,15 @@ import Nerv from 'nervjs'
 // import { Component, createElement } from 'nervjs'
  
 class Hello extends Nerv.Component {
+  // 构造器
   constructor () {
     super(...arguments)
     this.state = {
-      message: 'world'
+      message: '世界'
     }
   }
- 
+  
+  // 渲染
   render () {
     return (
       <div>
